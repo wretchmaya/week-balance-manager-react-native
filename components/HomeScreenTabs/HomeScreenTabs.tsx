d18d0740e428fcc48b75eadfc1dd25e2361fe4d2
Bohdan Triska <Bohdan_Triska@epam.com>
2023-05-09 01:18:05 +0300

    tabs view improve

--- components/HomeScreenTabs/HomeScreenTabs.tsx
+++ components/HomeScreenTabs/HomeScreenTabs.tsx
@@ -9,10 +9,29 @@ const Tab = createBottomTabNavigator();
 export const HomeScreenTabs = () => {
     return (
         <Tab.Navigator screenOptions={{ headerShown: false }}>
-            <Tab.Screen name={ROUTES.TABS.MAIN} component={MainTab} />
+            <Tab.Screen
+                name={ROUTES.TABS.MAIN}
+                component={MainTab}
+                options={{
+                    tabBarLabelStyle: {
+                        textAlign: 'center',
+                        marginBottom: 15,
+                        fontSize: 14,
+                    },
+                    tabBarIcon: () => null,
+                }}
+            />
             <Tab.Screen
                 name={ROUTES.TABS.FULL_HISTORY}
                 component={FullHistoryTab}
+                options={{
+                    tabBarLabelStyle: {
+                        textAlign: 'center',
+                        marginBottom: 15,
+                        fontSize: 14,
+                    },
+                    tabBarIcon: () => null,
+                }}
             />
         </Tab.Navigator>
     );
